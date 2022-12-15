46715438eeba9a9b647f3791b3e76cb4789f2e3c
trmrskr <simonalale@gmail.com>
2022-12-15 14:14:20 +0000

    Betty style and doc Correction

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,9 +1,15 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_size - calculates the size of a binary tree
+ * @tree: the tree whose size is to be calculated
+ * Return: the size of the given tree
+ */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	
+
 	return (binary_tree_size(tree->right) + binary_tree_size(tree->left) + 1);
 }
